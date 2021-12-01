50b0e967d850bcc68769d74a1c241a95c7edd303
StaishaKnight <staishaknight@gmail.com>
2021-12-01 12:52:22 -0600

    added style and outline for project

---
+++ script.js
@@ -0,0 +1,26 @@
+var buttonEl = document.querySelector("#start-quiz");
+var tasksToDoEl = document.querySelector("#start-quiz");
+
+
+//Start quiz//
+
+function startquiz (){}
+
+//start timer//
+
+function starttimer(){}
+
+//questions//
+
+function questionone(){}
+
+
+function displayanswer(){}
+
+//submit and show results//
+
+//all questions are answered or time reaches 0//
+
+//game over//
+
+//Save initials and score//
